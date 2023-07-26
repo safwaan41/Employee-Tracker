5738dc10d5413c149e7f0cf33fd69a075a2dfa52
safwaan41 <safwaan41@gmail.com>
2023-07-25 22:02:38 -0500

    added a few functions. more to go

--- db/schema.sql
+++ db/schema.sql
@@ -5,18 +5,18 @@ USE westeros_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name: VARCHAR(30) NOT NULL
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 // BOILER PLATE SERVER FOR SQL
 
-const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const PORT = process.env.PORT || 3001;
-const app = express();
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 const db = mysql.createConnection(
   {
@@ -18,7 +13,46 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the westeros_db database.`)
 );
+// ENQUIRER QUESTIONS
+function menu() {
+    inquirer.prompt([
+        {
+            message: 'What would you like to do?',
+            type: 'list',
+            name: 'introMenu',
+            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments','Add Department']
+        }
+    ])
+    .then(introMenuAns => {
+        if (introMenuAns.introMenu == 'View All Employees') {
+            viewAllEmployees();
+        }
+        if (introMenuAns.introMenu == 'Add Employee') {
+            addEmployee(); //MATCH FUNCTION LATER WITH THIS
+        }
+        if (introMenuAns.introMenu == 'Update Employee Role') {
+            updateEmployee();
+        }
+        if (introMenuAns.introMenu == 'View All Roles') {
+            viewAllRoles();
+        }
+        if (introMenuAns.introMenu == 'Add Role') {
+            addRole();
+        }
+        if (introMenuAns.introMenu == 'View All Departments') {
+            viewAllDep();
+        }
+        if (introMenuAns.introMenu == 'Add Department') {
+            addDep();
+        }
+    })
+}
+menu ();
 
+
+
+
+function viewAllDep() {
 // QUERY FOR department TABLE (1/3)
 db.query('SELECT * FROM department', function (err, results) {
   if (err) {
@@ -26,7 +60,11 @@ db.query('SELECT * FROM department', function (err, results) {
     return;
   }
   console.table(results);
+  menu ();
 });
+}
+
+function viewAllRoles() {
 // QUERY FOR role TABLE (2/3)
 db.query('SELECT * FROM role', function (err, results) {
     if (err) {
@@ -34,7 +72,11 @@ db.query('SELECT * FROM role', function (err, results) {
       return;
     }
     console.table(results);
+    menu ();
   });
+}
+
+function viewAllEmployees() {
 // QUERY FOR employee TABLE (3/3)
 db.query('SELECT * FROM employee', function (err, results) {
     if (err) {
@@ -42,12 +84,42 @@ db.query('SELECT * FROM employee', function (err, results) {
       return;
     }
     console.table(results);
+    menu ();
   });
+}
 
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+function addEmployee() {
+    inquirer.prompt([
+        {
+            message: 'What is the employees first name?',
+            type: 'input',
+            name: 'firstName',
+        },
+        {
+            message: 'What is the employees last name?',
+            type: 'input',
+            name: 'lastName',
+        },
+        {
+            message: 'What is the employees role ID?',
+            type: 'input',
+            name: 'roleID',
+        },
+        {
+            message: 'What is the employees manager ID?',
+            type: 'input',
+            name: 'managerID',
+        },
+    ])
+    .then(answers => {
+        db.query(`INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES( '${answers.firstName}', '${answers.lastName}', ${answers.roleID}, ${answers.managerID})`, function (err, results) {
+            if (err) {
+              console.log(err);
+              return;
+            }
+            console.table(results)
+            menu ();
+        })
+    })
+    
+}
